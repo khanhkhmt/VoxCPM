e198eda83848dd849a1186931a17c159b331b157
Thanh <Thanh2005.hs@gmail.com>
2026-05-13 04:24:00 +0000

    Add test api voice

--- web/next.config.ts
+++ web/next.config.ts
@@ -14,6 +14,19 @@ const nextConfig: NextConfig = {
         destination: 'http://127.0.0.1:8808/api/tts/file/:path*'
       }
     ]
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
+        ]
+      }
+    ]
   }
 };
 
